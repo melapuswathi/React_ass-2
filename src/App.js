aa61d75ba7bf498155d4c063e661dcb99eb2ca89
melapumounica <20b01a12a4@svecw.edu.in>
2022-02-12 11:58:08 +0530

    myfirstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import Faker from 'faker';
 
-function App() {
+const CardView = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <>
+      <div className="ui cards">
+        <div className="card">
+          <div className="content">
+            <img
+              className="right floated mini ui image"
+              src={Faker.image.image()}
+            />
+            <div className="header">Elliot Fu</div>
+            <div className="meta">Friends of Veronika</div>
+            <div className="description">
+              Elliot requested permission to view your contact details
+            </div>
+          </div>
+          <div className="extra content">
+            <div className="ui two buttons">
+              <div className="ui basic green button">Approve</div>
+              <div className="ui basic red button">Decline</div>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="content">
+            <img
+              className="right floated mini ui image"
+              src={Faker.image.image()}
+            />
+            <div className="header">Jenny Hess</div>
+            <div className="meta">New Member</div>
+            <div className="description">
+              Jenny wants to add you to the group <b>best friends</b>
+            </div>
+          </div>
+          <div className="extra content">
+            <div className="ui two buttons">
+              <div className="ui basic green button">Approve</div>
+              <div className="ui basic red button">Decline</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+};
+export default CardView;
